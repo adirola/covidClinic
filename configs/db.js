08bc25863e71bc0baf1e67dc4ed2fc07a8054e10
adirola <adirola@gmail.com>
2020-08-09 05:50:54 +0530

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secret': 'restapisecret',
-    'database': 'mongodb+srv://shreyas:shreyas3336@circuitapp-8wvpk.mongodb.net/userprofile?retryWrites=true&w=majority'
+    'secret': '',
+    'database': ''
   };
